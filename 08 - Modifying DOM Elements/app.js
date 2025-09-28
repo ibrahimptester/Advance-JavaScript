3f45af9820875e61e1282bb44208c525127589d2
ibrahimptester <ibrahimtareq159753@gmail.com>
2025-09-29 01:00:40 +0600

    Modifying DOM

---
+++ 08 - Modifying DOM Elements/app.js
@@ -0,0 +1,7 @@
+const heading = document.getElementsByTagName("h2")[0];
+heading.style.color = "Red"
+
+const lorem = document.querySelector(".lorem");
+lorem.style.backgroundColor = "lightgreen";
+lorem.style.padding = "15px"
+lorem.style.color = "#f2f2f2"
