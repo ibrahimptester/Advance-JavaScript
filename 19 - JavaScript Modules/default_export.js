b05abd79bf97d3e3b2b0eb385c9be5dd61f8e265
ibrahimptester <ibrahimtareq159753@gmail.com>
2025-10-05 00:59:37 +0600

    JavaScript Modules

---
+++ 19 - JavaScript Modules/default_export.js
@@ -0,0 +1,6 @@
+
+const greet = (name) => `Welcome ${name}`
+
+export default greet; // default export ( must be place below the exported thing )
+
+
---
+++ 19 - JavaScript Modules/import.js
@@ -0,0 +1,5 @@
+import { add } from "./app.js"; // namely import
+import greet from "./default_export.js"//default import
+
+console.log(add(2, 4))
+console.log(greet("Ibrahim"))
